ebd68fefe8a13335db16a7dbea0b7c4d7acc69a0
kmgeorgoudis-pixel <kmgeorgoudis@gmail.com>
2026-02-03 12:54:57 +0200

    upgrade 1

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,6 @@ function selectType(type) {
     // Μας μεταφέρει στη σελίδα του Forge για τον συγκεκριμένο τύπο (π.χ. url, pdf)
     window.location.href = "/forge/" + type;
 }
+.fa-file-zipper {
+    color: #eab308 !important; /* Χρυσαφί χρώμα για τα ZIP */
+}
